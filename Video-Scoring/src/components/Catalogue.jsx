40a5b1c71e8ab048439d56500a0dfca2f313c33c
Akshat <akshatpersonal321@gmail.com>
2025-01-14 17:36:27 +0530

    Catalogue page updated

--- Video-Scoring/src/components/Catalogue.jsx
+++ Video-Scoring/src/components/Catalogue.jsx
@@ -31,34 +31,33 @@ function Catalogue() {
   ];
   
   const collectionData2 = [
-    { id: 6, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1736773637/vqcjwqrqrfr0owgfln73.mp4" },
-    { id: 7, link: "https://res.cloudinary.com/dzz1r3hcf/image/upload/v1735449536/hybcxwkz2rtcw5vn5h3j.png" },
-    { id: 8, link: "https://res.cloudinary.com/dzz1r3hcf/image/upload/v1736559081/k57ojcvxltprrsbpo9zh.png" },
-    { id: 9, link: "https://res.cloudinary.com/dzz1r3hcf/image/upload/v1735450939/amozdnqtwvzgarhyzoha.png" },
-    { id: 10, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1736615039/bl807kjxvqdmfjdxy4pu.mp4" },
+    { id: 1, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1736773637/vqcjwqrqrfr0owgfln73.mp4" },
+    { id: 2, link: "https://res.cloudinary.com/dzz1r3hcf/image/upload/v1735449536/hybcxwkz2rtcw5vn5h3j.png" },
+    { id: 3, link: "https://res.cloudinary.com/dzz1r3hcf/image/upload/v1736559081/k57ojcvxltprrsbpo9zh.png" },
+    { id: 4, link: "https://res.cloudinary.com/dzz1r3hcf/image/upload/v1735450939/amozdnqtwvzgarhyzoha.png" },
+    { id: 5, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1736615039/bl807kjxvqdmfjdxy4pu.mp4" },
   ];
   
   const collectionData3 = [
-    { id: 11, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1736782583/gwkr5xvmeo3dainhuupw.mp4" },
-    { id: 12, link: "https://res.cloudinary.com/dzz1r3hcf/image/upload/v1736765557/acwwv03a0tiuw2yohuec.png" },
-    { id: 13, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1735026699/war4lxeqzycvrhid5jzp.mp4" },
-    { id: 14, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1735449989/mvrtza693wjjtps4ieyb.mp4" },
-    { id: 15, link: "https://res.cloudinary.com/dzz1r3hcf/image/upload/v1736729177/n1oqpjamozhfv0eghxd0.png" },
-    { id: 16, link: "https://v3.fal.media/files/koala/zjz8_QI9kDx1OYxC1LskL_output.mp4" },
+    { id: 1, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1736782583/gwkr5xvmeo3dainhuupw.mp4" },
+    { id: 2, link: "https://res.cloudinary.com/dzz1r3hcf/image/upload/v1736765557/acwwv03a0tiuw2yohuec.png" },
+    { id: 3, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1735026699/war4lxeqzycvrhid5jzp.mp4" },
+    { id: 4, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1735449989/mvrtza693wjjtps4ieyb.mp4" },
+    { id: 5, link: "https://res.cloudinary.com/dzz1r3hcf/image/upload/v1736729177/n1oqpjamozhfv0eghxd0.png" },
+    { id: 6, link: "https://v3.fal.media/files/koala/zjz8_QI9kDx1OYxC1LskL_output.mp4" },
   ];
   
   const collectionData4 = [
-    { id: 16, link: "https://res.cloudinary.com/dzz1r3hcf/image/upload/v1736777390/sojrl2h5etywpcuqeavf.png" },
-    { id: 17, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1735451420/ldc7o4k7des7cidsao1f.mp4" },
-    { id: 18, link: "https://res.cloudinary.com/dzz1r3hcf/image/upload/v1736728126/ajm3jplwemljzm0fhb4d.png" },
-    { id: 19, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1736559239/flp1nb70m1c0gmt8kidx.mp4" },
-    { id: 20, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1734993345/cwbdme61kqwmrxpauhj6.mp4" },
+    { id: 1, link: "https://res.cloudinary.com/dzz1r3hcf/image/upload/v1736777390/sojrl2h5etywpcuqeavf.png" },
+    { id: 2, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1735451420/ldc7o4k7des7cidsao1f.mp4" },
+    { id: 3, link: "https://res.cloudinary.com/dzz1r3hcf/image/upload/v1736728126/ajm3jplwemljzm0fhb4d.png" },
+    { id: 4, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1736559239/flp1nb70m1c0gmt8kidx.mp4" },
+    { id: 5, link: "https://res.cloudinary.com/dzz1r3hcf/video/upload/v1734993345/cwbdme61kqwmrxpauhj6.mp4" },
   ];
   
  
- function getClicked(index){
+ function getClicked(){
   setisClicked(true)
-  setClickedIndex (index)
  }
 
  addEventListener("scroll", () => {
@@ -80,11 +79,11 @@ function Catalogue() {
     <div className='ml-4 flex flex-col justify-between items-baseline p-1 h-screen w-[20vw]'>
        {
         collectionData1?.map((collection,index) => (<div 
-          onClick={()=>{getClicked(index)
-            setSelectedCollection(collectionData3)
+          onClick={()=>{getClicked(collection.id)
+            setSelectedCollection(collection)
           }}
           key={collection.id}
-        className= {`transition-all cursor-pointer overflow-clip w-[20vw] h-[24vh] m-1`}>
+        className= {`transition-all cursor-pointer hover:scale-[1.02] overflow-clip w-[20vw] h-[24vh] m-1`}>
 { (collection.link.split('.')).pop()==='png'?
               (<img src={collection.link} alt={collection.id} className='h-full w-full hover:shadow-sm hover:shadow-slate-100 border-2 border-gray-300 rounded-xl'/>):
               (<video
@@ -102,8 +101,8 @@ function Catalogue() {
 <div className='flex flex-col ml-4 justify-start items-center my-2 p-1 h-screen w-[25vw]'>
        {
         collectionData2?.map((collection,index) => (<div 
-          onClick={()=>{getClicked(index)
-            setSelectedCollection(collectionData2)
+          onClick={()=>{getClicked(collection.id)
+            setSelectedCollection(collection)
           }}
           key={collection.id} 
       className='overflow-clip cursor-pointer h-[32vh] w-[26vw] hover:scale-[1.02] m-1 transition-all'>
@@ -121,14 +120,14 @@ function Catalogue() {
   ))
 }
 </div>
-<div className='l-2 flex flex-col justify-between items-baseline p-1 h-screen w-[20vw]'>
+<div className=' flex flex-col justify-between items-baseline p-1 h-screen w-[20vw]'>
        {
         collectionData3?.map((collection,index) => (<div 
-          onClick={()=>{getClicked(index)
-            setSelectedCollection(collectionData3)
+          onClick={()=>{getClicked(collection.id)
+            setSelectedCollection(collection)
           }}
           key={collection.id}
-        className= {`transition-all cursor-pointer overflow-clip w-[20vw] h-[24vh] m-1`}>
+        className= {`transition-all cursor-pointer hover:scale-[1.02] overflow-clip w-[20vw] h-[24vh] m-1`}>
 { (collection.link.split('.')).pop()==='png'?
               (<img src={collection.link} alt={collection.id} className='h-full w-full hover:shadow-sm hover:shadow-slate-100 border-2 border-gray-300 rounded-xl'/>):
               (<video
@@ -146,8 +145,8 @@ function Catalogue() {
 <div className='flex flex-col justify-start items-center my-2 p-1 h-screen w-full mr-4'>
        {
         collectionData4?.map((collection,index) => (<div 
-          onClick={()=>{getClicked(index)
-            setSelectedCollection(collectionData4)
+          onClick={()=>{getClicked()
+            setSelectedCollection(collection)
           }}
           key={collection.id} 
         className='overflow-clip cursor-pointer h-[32vh] w-[26vw] hover:scale-[1.02] m-1 transition-all'>
@@ -168,16 +167,21 @@ function Catalogue() {
 
 {isClicked && (<div className={`fixed flex bg-center bg-no-repeat bg-cover items-center justify-center backdrop-blur-sm inset-0 text-white text-3xl z-50`}
 >
-  <div className='w-[90%] grid bg-no-repeat bg-cover border-4 rounded-xl border-r-white border-b-white border-t-black border-l-black bg-center  mt-[30px] h-3/4'
-  style={{
-    gridTemplateColumns: "repeat(20,minmax(0,1fr))",
-    gridTemplateRows: "repeat(20,minmax(0,1fr))",
-    backgroundImage: `url("${selectedCollection[clickedIndex]?.banner_image_url.replace('&', '/&')}")`}}
-  >
-  <div className='col-span-full col-start-auto row-span-2 flex justify-end'>
+    <div className='w-[85%] h-[85%]  relative flex justify-center items-center'>
+{ (selectedCollection.link.split('.')).pop()==='png'?
+              (<img src={selectedCollection.link} alt={selectedCollection.id} className='h-[70%] p-[2px]   hover:shadow-sm hover:shadow-slate-100 border-2 border-gray-300 rounded-xl'/>):
+              (<video
+              muted
+              autoPlay
+              loop
+              className='h-[70%] p-[2px]  hover:shadow-sm hover:shadow-slate-100 border-2 border-gray-300 rounded-xl'
+                src={selectedCollection.link}
+              ></video>)
+              }
+  <div className='col-span-full  col-start-auto row-span-2 flex justify-end'>
   <FontAwesomeIcon
   onClick={()=>setisClicked(false)}
-  className=' cursor-pointer text-[#12263f] absolute transform translate-x-1/2 -translate-y-1/2' icon={faXmarkCircle}/>
+  className=' cursor-pointer text-[#12263f] top-20 right-[240px] absolute transform' icon={faXmarkCircle}/>
   </div>
   </div>
 

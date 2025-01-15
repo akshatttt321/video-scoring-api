c74e4e225318e7957880b28ef20cff650602c202
Akshat <akshatpersonal321@gmail.com>
2025-01-16 00:08:49 +0530

    fix bug with deployment

--- Video-Scoring/src/main.jsx
+++ Video-Scoring/src/main.jsx
@@ -1,35 +1,41 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
 import Dashboard from './components/Dashboard.jsx';
 import HomePage from './components/Home.jsx';
+import { SharedProvider } from './sharedContext.jsx';
 import InputForm from './components/InputForm.jsx';
+import { useSharedContext } from './sharedContext.jsx';
+import { useParams } from 'react-router-dom';
 import Catalogue from './components/Catalogue.jsx';
 
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
   {
-    path: "",
-    element: <HomePage />,
+    path: "/",
+    element: <HomePage />
   },
   {
-    path: "/input-form",
-    element: <InputForm />,
+    path:"/input-form",
+    element:<InputForm />
   },
   {
-    path: "/dashboard/:id",
-    element: <Dashboard />,
+    path: `/dashboard/:id`,
+    element: <Dashboard />
   },
   {
-    path: "/catalogue",
-    element: <Catalogue />,
-  },
+    path: `/catalogue`,
+    element: <Catalogue />
+  }
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <SharedProvider>
+      <RouterProvider router={router} />
+    </SharedProvider>
+  </StrictMode>
 );
-
